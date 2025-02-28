6d073dfb404dba9b23324e4c7c796b212a2e7dca
Zachary Parker <zaxparker@gmail.com>
2025-02-28 13:22:47 +0300

    Completed building months calculator, did minor code clean up made a few visual changes.

--- main.js
+++ main.js
@@ -2,16 +2,16 @@
 let loan_amount;
 let interest_rate;
 let payment_amount;
-let number_of_months;
+let select_input;
 let months_years;
 
 
-
+//function retrieves data from html inputs
 function get_data() {
 //Retrieves values of html inputs using html class selectors
 loan_amount = document.querySelector(".loan_amount_input").value;
 interest_rate = document.querySelector(".interest_rate_input").value;
-number_of_months = document.querySelector(".select_input").value;
+select_input = document.querySelector(".select_input").value;
 select = document.querySelector(".select").value;
 months_years = document.querySelector(".select_two").value;
 
@@ -19,8 +19,7 @@ months_years = document.querySelector(".select_two").value;
 //Converts all values of html inputs into floats
 loan_amount = parseFloat(loan_amount);
 interest_rate = parseFloat(interest_rate);
-number_of_months = parseFloat(number_of_months);
-payment_amount = parseFloat(payment_amount);
+select_input = parseFloat(select_input);
 
 //If user selects calculate payment runs function to calculate payment.
 //If user selects calculate months runs function to calculate number of months.
@@ -32,31 +31,52 @@ calculate_months();
 }
 
 
-
+//function calculates monthly payment
 function calculate_payment() {
+//declares variable number_of_months
+let number_of_months;
 
 //if years are slected instead of months. Converts years to months.
 if (months_years == "years") {
-number_of_months = number_of_months*12;
+number_of_months = select_input*12;
 } 
+else {
+number_of_months = select_input;  
+}
 
 //adjusted intrest rate
-let interest_rate_adjusted = interest_rate/12
+let interest_rate_adjusted = interest_rate/1200
 
+let temp_one = 1+interest_rate_adjusted;
+let temp_two = Math.pow(temp_one, -number_of_months);
+let temp_three = 1 - temp_two;
+let monthly_payment = loan_amount * (interest_rate_adjusted/temp_three);
+//rounds final calculation to two decimal places
+ monthly_payment = monthly_payment.toFixed(2);
 
-let formula = 1+interest_rate_adjusted;
-formula = Math.pow(formula, -number_of_months)
-formula = 1 - formula
-
-let monthly_payment = loan_amount * (interest_rate_adjusted/formula);
-
-alert(monthly_payment);
+ //selects result html element and changes to calculation 
+ let result_display = document.querySelector(".result_text");
+ result_display.textContent = "Payment Per Month:  $" + monthly_payment;
 }
 
 
-
+//function calculates number of months based on set monthly payment
 function calculate_months() {
+    //sets payment_amount equal to data in select_input
+	let payment_amount = select_input
 
-alert("ran calculate months function");
+    //adjust interest rate from percentage to decimal
+    interest_rate_adjusted = interest_rate/1200;
 
+    //formula to find loan months
+    let temp_zero =  1 - (loan_amount/payment_amount) * interest_rate_adjusted;
+    let temp_one = Math.log(temp_zero);
+    let temp_two = Math.log(1 + interest_rate_adjusted);
+    loan_months = -(temp_one/temp_two);
+    //rounds final calculation up to nearest whole number
+    loan_months = Math.ceil(loan_months);
+
+    //selects result html element and changes to calculation 
+    let result_display = document.querySelector(".result_text");
+    result_display.textContent = loan_months + " Months";
 }
